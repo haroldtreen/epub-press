946394dfacbeb9c4e7b3fba949a43da291bc39e7
Harold Treen <haroldtreen@gmail.com>
2016-02-26 19:41:17 -0500

    Working with Chrome extension

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'chrome-extension://jknlfjapjepbmcodmplkccokhkmecinp');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
+
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const epubGenerator = require('../lib/epub-generator');
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/api/books/publish', (req, res) => {
+    console.log(req.body['urls[]']);
+    if (req.body['urls[]']) {
+        epubGenerator.fromUrls(`Book-${Date.now()}`, req.body['urls[]']).then((id) => {
+            res.json({ bookId: id });
+        });
+    } else {
+        res.end();
+    }
+});
+
+router.get('/api/books/download', (req, res) => {
+    if (req.query.bookId) {
+        res.download(`ebooks/example-ebook${req.query.bookId}.epub`);
+    }
 });
 
 module.exports = router;
