5061af6959c31843eeb78115a11e93249e8655cb
Harold Treen <haroldtreen@gmail.com>
2016-02-28 22:30:11 -0500

    Update endpoints for creating and retrieving ebooks

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const epubGenerator = require('../lib/epub-generator');
+const BookServices = require('../lib/book-services');
+const Book = require('../lib/book');
 const express = require('express');
 const router = express.Router();
 
@@ -7,20 +8,28 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Express' });
 });
 
-router.post('/api/books/publish', (req, res) => {
-    console.log(req.body['urls[]']);
-    if (req.body['urls[]']) {
-        epubGenerator.fromUrls(`Book-${Date.now()}`, req.body['urls[]']).then((id) => {
-            res.json({ bookId: id });
-        });
+router.post('/api/books', (req, res) => {
+    console.log(req.body.urls);
+    if (req.body.urls) {
+        const book = new Book({ urls: req.body.urls });
+
+        BookServices.updateSectionsHtml(book).then((updatedBook) =>
+            BookServices.extractSectionsContent(updatedBook)
+        ).then((updatedBook) =>
+            BookServices.convertSectionsContent(updatedBook)
+        ).then((updatedBook) =>
+            updatedBook.writeEpub()
+        ).then((writtenBook) =>
+            res.json({ id: writtenBook.filename() })
+        );
     } else {
         res.end();
     }
 });
 
 router.get('/api/books/download', (req, res) => {
-    if (req.query.bookId) {
-        res.download(`ebooks/example-ebook${req.query.bookId}.epub`);
+    if (req.query.id) {
+        res.download(`ebooks/${req.query.id}.epub`);
     }
 });
 
