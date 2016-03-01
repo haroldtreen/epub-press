8b548f21b1bb0b5d3fa26da9e0de27733a96c433
Harold Treen <haroldtreen@gmail.com>
2016-03-01 16:38:48 -0500

    Fix cross domain errors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'chrome-extension://jknlfjapjepbmcodmplkccokhkmecinp');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
