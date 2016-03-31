f839df95ff833c97310b398fdaa4a88f0561bdb5
Harold Treen <haroldtreen@gmail.com>
2016-03-31 15:27:33 -0500

    Use nginx compression instead of express compression

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var compress = require('compression');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(compress());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
