d82247790a5697cabfc23c3e9ef81ef07148877f
Harold Treen <haroldtreen@gmail.com>
2016-04-08 14:03:34 -0500

    Increase app limits in preparation for receiving html.

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
