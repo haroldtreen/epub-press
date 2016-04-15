d5b2475309a30839a09dd5faf85f8fb5741e1572
Harold Treen <haroldtreen@gmail.com>
2016-04-14 22:04:50 -0500

    Fix model loader code

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${__dirname}/../config/config`)[env];
 const db = {};
 let sequelize;
 
