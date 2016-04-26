a2153871f47591fa81674fafefb2fa6382ccd916
Harold Treen <haroldtreen@gmail.com>
2016-04-25 23:39:00 -0500

    Separate test file for article tests.

--- tests/content-extractor-test.js
+++ tests/content-extractor-test.js
@@ -5,32 +5,6 @@ const assert = require('chai').assert;
 const ContentExtractor = require('../lib/content-extractor');
 
 describe('Content Extractor', () => {
-    describe('extract', () => {
-        it('can extract a new york times articles', (done) => {
-            ContentExtractor.extract(
-                fs.readFileSync(`${__dirname}/fixtures/articles/new-york-times.html`).toString()
-            ).then((article) => {
-                assert.notInclude(article.content, '<script>');
-                assert.match(article.content, /BARF/);
-                assert.equal(
-                    article.title,
-                    'In Cramped and Costly Bay Area, Cries to Build, Baby, Build'
-                );
-                done();
-            }).catch(done);
-        }).timeout(2500);
-
-        it('can extract a blogspot article', (done) => {
-            ContentExtractor.extract(
-                fs.readFileSync(`${__dirname}/fixtures/articles/blogspot.html`).toString()
-            ).then((article) => {
-                assert.include(article.content, 'End the life of the hardware (brick it)');
-                assert.include(article.content, 'Anytime you work on a software project,');
-                done();
-            }).catch(done);
-        }).timeout(2500);
-    });
-
     describe('preprocess', () => {
         it('should bubble up no script content', (done) => {
             const html = fs.readFileSync(`${__dirname}/fixtures/noscript.html`).toString();
@@ -56,7 +30,7 @@ describe('Content Extractor', () => {
                 ContentExtractor.preprocess.REMOVE_ELEMENTS.forEach((element) => {
                     assert.notMatch(preHtml, new RegExp(element));
                 });
-                done()
+                done();
             }).catch(done);
         });
     });
