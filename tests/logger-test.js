0b8e39d73f9ec5178c7842d270b2879e415b2d02
Harold Treen <haroldtreen@gmail.com>
2016-04-29 21:05:48 -0500

    Adds logger + tests

--- tests/logger-test.js
+++ tests/logger-test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const assert = require('chai').assert;
 
 const Logger = require('../lib/logger');
@@ -8,7 +10,11 @@ let log;
 describe('Logger', () => {
     describe('basic usage', () => {
         before(() => {
-            log = new Logger();
+            log = new Logger({ options: [Logger.Console] });
+        });
+
+        after(() => {
+            fs.unlinkSync(Logger.outputFile());
         });
 
         it('defines all the basic log levels', () => {
@@ -19,7 +25,56 @@ describe('Logger', () => {
 
         it('has default option', () => {
             const defaultOptions = log.getOptions();
-            assert.deepEqual(defaultOptions.outputs, [Logger.Console]);
+            assert.deepEqual(defaultOptions.outputs, [Logger.Console, Logger.File]);
+        });
+
+        it('logs to a file', (done) => {
+            let calls = 0;
+            const errorMsg = 'This is an error message';
+            const infoMsg = 'This is an info message';
+            const metadata = { meta: true };
+
+            const msgLogged = () => {
+                setTimeout(() => {
+                    calls++;
+                    if (calls === 2) {
+                        fs.readFile(Logger.outputFile(), (err, data) => {
+                            const output = data.toString();
+
+                            assert.include(output, '"meta":true');
+                            assert.include(output, errorMsg);
+                            assert.include(output, infoMsg);
+                            done();
+                        });
+                    }
+                }, 1);
+            };
+
+            const fileLogger = new Logger({ outputs: [Logger.File] });
+
+            fileLogger.error(errorMsg, metadata, msgLogged);
+            fileLogger.info(infoMsg, metadata, msgLogged);
+        });
+
+        it('creates exception loggers', (done) => {
+            const fileLogger = new Logger({ outputs: [Logger.File] });
+
+            fileLogger.on('logging', () => {
+                fs.readFile(Logger.outputFile(), (err, data) => {
+                    const output = data.toString();
+
+                    ['a promise', 'some horrible error', 'logger-test'].forEach(
+                        (msg) => assert.include(output, msg)
+                    );
+                    done();
+                });
+            });
+
+            new Promise((resolve, reject) => {
+                reject(new Error('some horrible error!'));
+            }).then(() => {}).catch(fileLogger.exception('a promise'));
+
+
         });
     });
 
