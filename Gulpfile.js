1abd96ce49e62a09509366cfb41e8afde28cdc89
Harold Treen <haroldtreen@gmail.com>
2016-04-29 21:04:46 -0500

    Break tests up by speed + type.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,14 +3,26 @@ require('harmonize')();
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
-gulp.task('default', () => {
-    return gulp.src('./tests/*-test.js', { read: false })
-        // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({ reporter: 'spec', harmony: true }));
+gulp.task('test', () => {
+    process.env.NODE_ENV = 'test';
+    return gulp.src('./tests/**/*-test.js', { read: false })
+			.pipe(mocha({ reporter: 'spec', harmony: true }));
 });
 
 gulp.task('test-db', () => {
-	process.env.NODE_ENV = 'test';
+    process.env.NODE_ENV = 'test';
     return gulp.src('./tests/models/*-test.js', { read: false })
 			.pipe(mocha({ reporter: 'spec', harmony: true }));
+});
+
+gulp.task('test-unit', () => {
+    process.env.NODE_ENV = 'test';
+    return gulp.src('./tests/*-test.js', { read: false })
+            .pipe(mocha({ reporter: 'spec', harmony: true }));
+});
+
+gulp.task('test-integration', () => {
+    process.env.NODE_ENV = 'test';
+    return gulp.src('./tests/integration/*-test.js', { read: false })
+            .pipe(mocha({ reporter: 'spec', harmony: true }));
 });
