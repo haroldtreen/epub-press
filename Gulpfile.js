3ef40e51feb6b8bc5479c9a6b5f8efd42be69801
Harold Treen <haroldtreen@gmail.com>
2016-05-10 21:19:24 -0500

    Allow regex in test tasks. Point to readability master.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,29 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const args = require('yargs').argv;
+
+const options = { reporter: 'spec', harmony: true, grep: args.regex || '.' };
 
 gulp.task('test', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/**/*-test.js', { read: false })
-			.pipe(mocha({ reporter: 'spec', harmony: true }));
+			.pipe(mocha(options));
 });
 
 gulp.task('test-db', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/models/*-test.js', { read: false })
-			.pipe(mocha({ reporter: 'spec', harmony: true }));
+			.pipe(mocha(options));
 });
 
 gulp.task('test-unit', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/*-test.js', { read: false })
-            .pipe(mocha({ reporter: 'spec', harmony: true }));
+            .pipe(mocha(options));
 });
 
 gulp.task('test-integration', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/integration/*-test.js', { read: false })
-            .pipe(mocha({ reporter: 'spec', harmony: true }));
+            .pipe(mocha(options));
 });
