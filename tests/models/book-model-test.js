bc8ca8d830833179966ac772a07b972a8e1edd25
Harold Treen <haroldtreen@gmail.com>
2016-06-09 01:44:19 -0500

    Fix test

--- tests/models/book-model-test.js
+++ tests/models/book-model-test.js
@@ -5,7 +5,7 @@ const BookModel = require('../../models/').Book;
 describe('Book Model', () => {
     it('can create books', (done) => {
         const attrs = {
-            title: 'Test Book',
+            title: 'A Test Book',
             sections: [{ title: 'Section 1', url: 'https://epub.press' }],
         };
         BookModel.create(attrs).then(() =>
