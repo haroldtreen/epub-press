1169ee8169adec92966008fec7828e3fb6014cf1
Harold Treen <haroldtreen@gmail.com>
2017-02-15 00:43:02 -0800

    Small refactors

--- tests/stress.js
+++ tests/stress.js
@@ -1,14 +1,15 @@
+/* eslint no-console: 0 */
 const loadtest = require('loadtest');
 
-function statusCallback(latency, result, error) {
-    console.log('Current latency %j, result %j', latency, error ? JSON.stringify(error) + result.toString() : result);
+function statusCallback(error, result = {}, latency = {}) {
+    console.log('Latency:');
+    console.log(latency);
+    console.log('Result:');
+    console.log(result);
     console.log('----');
-    console.log('Request elapsed milliseconds: ', error ? error.requestElapsed : result.requestElapsed);
-    console.log('Request index: ', error ? error.requestIndex : result.requestIndex);
-    console.log('Request loadtest() instance index: ', error ? error.instanceIndex : result.instanceIndex);
 }
 
-var urls = [
+const urls = [
     'https://github.com/injectivejs/injective',
     'http://www.cbc.ca/news/canada/ottawa/workplace-sexual-discrimination-men-heels-union-613-1.3483305?cmp=rss',
     'http://appleinsider.com/articles/16/03/10/apple-issues-sixth-beta-of-tvos-92-for-apple-tv-to-developers',
@@ -51,50 +52,24 @@ var urls = [
     'http://www.cbc.ca/news/canada/north/trudeau-obama-washington-visit-arctic-promises-1.3486076?cmp=rss',
 ];
 
-// function(params, options, client, callback) {
-//     request = client(options, callback);
-//
-//     if (message)
-//     {
-//       options.headers['Content-Length'] = message.length;
-//       options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-//       request.write(message);
-//     }
-//
-//     request.end();
-// }
-
-
 const options = {
     statusCallback,
     // requestGenerator,
     method: 'post',
     concurrency: 6,
     body: {
-        urls: [
-            'http://www.cbc.ca/news/technology',
-            'http://www.cbc.ca/news/canada/calgary/john-ridsdel-hostage-trudeau-1.3487367',
-            'http://www.cbc.ca/news/technology/forum-energy-new-technology-jobs-las-vegas-1.3487613',
-            'http://www.cbc.ca/news/technology/bangladesh-bank-typo-hack-new-york-fed-1.3485125',
-            'http://www.cbc.ca/news/technology/mercury-dark-1.3481517',
-            'http://www.cbc.ca/news/technology/esports-cineplex-call-of-duty-tournament-1.3471115',
-            'https://www.thestar.com/news/world/2016/03/11/worlds-oldest-man-112-confesses-i-dont-know-the-secret.html',
-            'http://www.huffingtonpost.ca/karri-munnvenn/vancouver-climate-declaration_b_9432700.html',
-            'http://www.cbc.ca/news/world/germany-islamic-state-files-1.3484861',
-            'https://www.thestar.com/news/world/2016/03/11/ben-carson-backs-donald-trump-buries-the-hatchet-with-former-rival.html',
-        ],
+        urls,
     },
     url: 'http://localhost:3000/api/books',
     contentType: 'application/json',
     maxRequests: 300,
 };
 
-loadtest.loadTest(options, function(error, result)
-{
-    if (error)
-    {
+loadtest.loadTest(options, (error, result) => {
+    if (error) {
         return console.error('Got an error: %s', error);
     }
     console.log('Tests run successfully');
-	console.log(result);
+    console.log(result);
+    return undefined;
 });
