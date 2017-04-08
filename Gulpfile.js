59569764ece37db9402a5cd2ce4e813d9ae68baa
Harold Treen <haroldtreen@gmail.com>
2017-04-08 08:17:54 -0400

    Flow and linting fixes.

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,35 +8,35 @@ const options = { reporter: 'spec', harmony: true, grep: args.regex || '.' };
 gulp.task('pre-test', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src(['lib/**/*.js', 'models/*.js', 'routes/**/*.js'])
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire());
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], () =>
     gulp.src('./tests/**/*-test.js', { read: false })
-			.pipe(mocha(options))
-            .pipe(istanbul.writeReports())
+        .pipe(mocha(options))
+        .pipe(istanbul.writeReports())
 );
 
 gulp.task('test-db', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/models/*-test.js', { read: false })
-			.pipe(mocha(options));
+    .pipe(mocha(options));
 });
 
 gulp.task('test-unit', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/*-test.js', { read: false })
-            .pipe(mocha(options));
+    .pipe(mocha(options));
 });
 
 gulp.task('test-integration', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src('./tests/integration/**/*-test.js', { read: false })
-            .pipe(mocha(options));
+    .pipe(mocha(options));
 });
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    console.log('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
     // application specific logging, throwing an error, or other logic here
 });
