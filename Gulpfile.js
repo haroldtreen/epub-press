6e0ee45f02362dd96c09b54f73ca8825d27800c8
Harold Treen <haroldtreen@gmail.com>
2017-05-28 20:04:04 -0400

    Add a test script and test-ci task.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,39 +1,41 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const istanbul = require('gulp-istanbul');
+const sequence = require('gulp-sequence');
 const args = require('yargs').argv;
 
 const options = { reporter: 'spec', harmony: true, grep: args.regex || '.' };
 
 gulp.task('pre-test', () => {
     process.env.NODE_ENV = 'test';
-    return gulp.src(['lib/**/*.js', 'models/*.js', 'routes/**/*.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+    return gulp
+        .src(['lib/**/*.js', 'models/*.js', 'routes/**/*.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], () =>
-    gulp.src('./tests/**/*-test.js', { read: false })
+    gulp
+        .src('./tests/**/*-test.js', { read: false })
         .pipe(mocha(options))
         .pipe(istanbul.writeReports())
 );
 
+gulp.task('test-ci', sequence('test-unit', 'test-integration'));
+
 gulp.task('test-db', () => {
     process.env.NODE_ENV = 'test';
-    return gulp.src('./tests/models/*-test.js', { read: false })
-    .pipe(mocha(options));
+    return gulp.src('./tests/models/*-test.js', { read: false }).pipe(mocha(options));
 });
 
 gulp.task('test-unit', () => {
     process.env.NODE_ENV = 'test';
-    return gulp.src('./tests/*-test.js', { read: false })
-    .pipe(mocha(options));
+    return gulp.src('./tests/*-test.js', { read: false }).pipe(mocha(options));
 });
 
 gulp.task('test-integration', () => {
     process.env.NODE_ENV = 'test';
-    return gulp.src('./tests/integration/**/*-test.js', { read: false })
-    .pipe(mocha(options));
+    return gulp.src('./tests/integration/**/*-test.js', { read: false }).pipe(mocha(options));
 });
 
 process.on('unhandledRejection', (reason, p) => {
