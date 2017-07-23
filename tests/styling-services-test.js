da3245f85c4b37b78798fad0bd0bf8f3840bf9d7
Harold Treen <haroldtreen@gmail.com>
2017-07-23 16:24:25 -0400

    Ability to write on book covers.

--- tests/styling-services-test.js
+++ tests/styling-services-test.js
@@ -1,13 +1,23 @@
 const assert = require('chai').assert;
+
+const fs = require('fs-extra');
+
 const Book = require('../lib/book');
 const StylingService = require('../lib/styling-service');
 
+function assertDifferentFile(path1, path2) {
+    assert.notEqual(path1, path2);
+    assert.isTrue(fs.pathExistsSync(path1));
+    assert.isTrue(fs.pathExistsSync(path2));
+}
+
 describe('StylingService', () => {
     it('can add text to a book cover', () => {
-        let book = new Book();
+        const book = new Book({ title: 'This is a book' });
         const preStyledCover = book.getCoverPath();
-        book = StylingService.writeOnCover(book, 'Hello World');
-        const postStyledCover = book.getCoverPath();
-        assert.notEqual(preStyledCover, postStyledCover);
+        return StylingService.writeOnCover(book, 'Hello World').then((updatedBook) => {
+            const postStyledCover = updatedBook.getCoverPath();
+            assertDifferentFile(preStyledCover, postStyledCover);
+        });
     });
 });
