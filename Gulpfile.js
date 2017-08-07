2649d06a1b369545c3ea4a2ebe0548102b0c9337
Harold Treen <haroldtreen@gmail.com>
2017-08-06 21:41:23 -0400

    Prettify gulpfile.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,38 +4,36 @@ const istanbul = require('gulp-istanbul');
 const sequence = require('gulp-sequence');
 const args = require('yargs').argv;
 
-const options = { reporter: 'spec', harmony: true, grep: args.regex || '.' };
+const options = { reporter: 'spec', grep: args.regex || '.' };
 
 gulp.task('pre-test', () => {
     process.env.NODE_ENV = 'test';
     return gulp
-    .src(['lib/**/*.js', 'models/*.js', 'routes/**/*.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+        .src(['lib/**/*.js', 'models/*.js', 'routes/**/*.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], () => {
     gulp
-    .src('./tests/**/*-test.js', { read: false })
-    .pipe(mocha(options))
-    .pipe(istanbul.writeReports());
+        .src('./tests/**/*-test.js', { read: false })
+        .pipe(mocha(options))
+        .pipe(istanbul.writeReports());
 });
 
 gulp.task('test-ci', sequence('test-unit', 'test-integration'));
 
 gulp.task('test-db', () => {
     process.env.NODE_ENV = 'test';
-    return gulp
-    .src('./tests/models/*-test.js', { read: false })
-    .pipe(mocha(options));
+    return gulp.src('./tests/models/*-test.js', { read: false }).pipe(mocha(options));
 });
 
 gulp.task('test-unit', () => {
     process.env.NODE_ENV = 'test';
     return gulp
-    .src('./tests/*-test.js', { read: false })
-    .pipe(mocha(options))
-    .on('error', () => {});
+        .src('./tests/*-test.js', { read: false })
+        .pipe(mocha(options))
+        .on('error', () => {});
 });
 
 gulp.task('watch-test-unit', () => {
@@ -45,12 +43,5 @@ gulp.task('watch-test-unit', () => {
 
 gulp.task('test-integration', () => {
     process.env.NODE_ENV = 'test';
-    return gulp
-    .src('./tests/integration/**/*-test.js', { read: false })
-    .pipe(mocha(options));
-});
-
-process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise ', p, ' reason: ', reason);
-  // application specific logging, throwing an error, or other logic here
+    return gulp.src('./tests/integration/**/*-test.js', { read: false }).pipe(mocha(options));
 });
